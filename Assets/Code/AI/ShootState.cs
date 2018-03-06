9e0ff0c5a6edc34325f9d46402236b00bc67ffec
JerriAhonen <jerri.ahonen@cs.tamk.fi>
2018-03-06 17:30:33 +0200

    Assignement 2. Added ShootState.cs, modified EnemyUnit.cs.

---
+++ Assets/Code/AI/ShootState.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame.AI
+{
+    public class ShootState : AIStateBase
+    {
+        public float SqrShootingDistance
+        {
+            get { return Owner.ShootingDistance * Owner.ShootingDistance; }
+        }
+
+        public ShootState(EnemyUnit owner)
+            : base(owner, AIStateType.Shoot)
+        {
+            AddTransition(AIStateType.Patrol);
+            AddTransition(AIStateType.FollowTarget);
+        }
+
+        public override void Update()
+        {
+            if (!ChangeState())
+            {
+                Owner.Mover.Move(Owner.transform.forward);                  // Moving
+                Owner.Mover.Turn(Owner.Target.transform.position);          // Turning
+                Owner.Weapon.Shoot();                                       // Shooting
+            }
+        }
+
+        private bool ChangeState()
+        {
+            Vector3 toPlayerVector = Owner.transform.position - Owner.Target.transform.position;
+            float sqrDistanceToPlayer = toPlayerVector.sqrMagnitude;
+            
+            // If player dead, go to patrol state
+            if (Owner.Target.gameObject.activeSelf == false)                // Check if player activeSelf false. If yes, player dead.
+            {
+                Owner.Target = null;                                        // Set the target to null
+                return Owner.PerformTransition(AIStateType.Patrol);         // Go to Patrol state
+            }
+            
+            // If not at shooting range, go to follow state.
+            if (sqrDistanceToPlayer > SqrShootingDistance)
+            {
+                return Owner.PerformTransition(AIStateType.FollowTarget);   // Go to Follow state
+            }
+
+            // Otherwise return false.
+            return false;
+        }
+    }
+}
---
+++ Assets/Code/CameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame
+{
+    public class CameraFollow : MonoBehaviour, ICameraFollow
+    {
+        public Transform target;
+
+        public float distance;
+        public float angle;
+
+        Vector3 offset;
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Calculate the offset position of the Camera relative to the player's position.
+            offset = target.position - transform.forward * distance;
+            transform.position = offset;
+
+            //Rotate the Camera according to the player, and the set angle.
+            //90 - angle, so that we get the desired angle to the inspector. 
+            //(when we insert 90, the camera is on the ground, and when 0 the camera is on top of the player.)
+            Quaternion localRotation = Quaternion.Euler(90.0f - angle, target.rotation.eulerAngles.y, 0.0f);
+            transform.rotation = localRotation;
+        }
+        
+        public void SetAngle(float angle)
+        {
+            this.angle = angle;
+        }
+
+        public void SetDistance(float distance)
+        {
+            this.distance = distance;
+        }
+
+        public void SetTarget(Transform targetTransform)
+        {
+            target = targetTransform;
+        }
+    }
+}
